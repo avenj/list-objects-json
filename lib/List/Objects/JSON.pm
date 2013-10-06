629e0a9b924ed1721353e3b21c592cc451997b34
Jon Portnoy <avenj@cobaltirc.org>
2013-10-06 05:05:40 -0400

    POD

--- lib/List/Objects/JSON.pm
+++ lib/List/Objects/JSON.pm
@@ -33,8 +33,12 @@ List::Objects::JSON - Wrap List::Objects::WithUtils around JSON::Tiny
   my $js = List::Objects::JSON->new;
 
   my $data  = array(qw/ foo bar baz /);
+
   my $bytes = $js->encode( $data->grep(sub { /^b/ }) );
+
   my $array = $js->decode($bytes);
+  my $count = $array->count;  # 2
+
 
 =head1 DESCRIPTION
 
@@ -52,13 +56,13 @@ C<TO_JSON> method, but I found myself frequently typing something like:
 A subclass can override C<_decode_listobj_array> and C<_decode_listobj_hash>
 appropriately to produce other object types. For example:
 
-  # A subclass producing immutable data structures.
+  # A subclass producing immutable data structures:
   package My::JSON::Immutable;
   use strictures 1;
   use List::Objects::WithUtils 2;
   use parent 'List::Objects::JSON';
 
-  sub _decode_listobj_array {
+  sub _decode_listobj_array { 
     my ($self, $item) = @_;
     immarray(@$item)
   }
@@ -68,6 +72,17 @@ appropriately to produce other object types. For example:
     immhash(%$item)
   }
 
+  # A subclass that type-checks arrays:
+  package My::JSON::CheckInt;
+  use strictures 1;
+  use List::Objects::WithUtils 2;
+  use Types::Standard 'Int';
+  use parent 'List::Objects::JSON';
+
+  sub _decode_listobj_array {
+    my ($self, $item) = @_;
+    array_of Int() => @$item
+  }
 
 =head1 AUTHOR
 
